de8e1d14b361d7dee616ac7e17d1e4990651a3d5
kyaldabomb <62745708+kyaldabomb@users.noreply.github.com>
2025-03-06 12:48:05 +1100

    Create scrapers_config.py

---
+++ scrapers_config.py
@@ -0,0 +1,14 @@
+# Define all scrapers and their configurations
+SCRAPERS = {
+    "belfield": {
+        "file_name": "Belfield.xlsx", 
+        "script_name": "Belfield_Daily_Modified.py",
+        "description": "Belfield Music Store"
+    },
+    "belfield_monthly": {
+        "file_name": "Belfield.xlsx",
+        "script_name": "Belfield_Daily_Modified.py", 
+        "description": "Belfield Music Store"
+    },
+    
+}
