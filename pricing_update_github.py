1cbdb925911b791672d9d48d98eb25139d7b03c6
kyaldabomb <62745708+kyaldabomb@users.noreply.github.com>
2026-01-05 09:38:45 +1100

    Fix discount calculation in pricing_update_github.py
    Adjust default discount and cost calculation for pricing.

--- pricing_update_github.py
+++ pricing_update_github.py
@@ -2504,7 +2504,7 @@ def Marshall(RRP, title, sku, obsolete_stock):
 
     obsolete_stock = obsolete_stock
     title = title
-    default_discont = (0.9)*0.9
+    default_discont = (0.9)
 
     if 'fridge' in title.lower():
         cost = (RRP * 0.75)
@@ -2512,7 +2512,7 @@ def Marshall(RRP, title, sku, obsolete_stock):
 
     else:
 
-        cost = (RRP * 0.7)*0.8
+        cost = (RRP * 0.7)*0.9
 
     if RRP >= 100:
         return RRP * default_discont, cost
